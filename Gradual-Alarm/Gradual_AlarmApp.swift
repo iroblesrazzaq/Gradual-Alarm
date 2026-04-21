6cbd8557559b1bb84617925b18d1b402bde6eb1b
Ismael Robles-Razzaq <146605996+iroblesrazzaq@users.noreply.github.com>
2026-04-21 10:36:13 +0200

    Initial Commit

---
+++ Gradual-Alarm/Gradual_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gradual_AlarmApp.swift
+//  Gradual-Alarm
+//
+//  Created by Ismael Robles-Razzaq on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Gradual_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
